2c5ebfe09a834c0c8b815a45ffe27ff600fe8131
ant77i <leobor2007@gmail.com>
2024-11-25 17:32:38 +0100

    Finished 15 and 7 in zad6

--- zad6/zad6.sql
+++ zad6/zad6.sql
@@ -85,19 +85,59 @@ FROM Production.Product as P
 GROUP BY P.Name
 --
 
--- zad. 7 niedokonczone
+-- zad. 7 na dwa sposoby 
 SELECT
 	P.Name,
-	DATEPART(year, SOH.OrderDate) as Rok,
-	SUM(SOD.OrderQty) / (SELECT SUM(SOD.OrderQty) FROM Production.Product WHERE DATEPART(year, SOH.OrderDate)+1 = Rok)
+	YEAR(SOH.OrderDate) AS Rok,
+	--SUM(SOD.LineTotal),
+	--LAG(SUM(SOD.OrderQty)) OVER (PARTITION BY P.ProductID ORDER BY YEAR(SOH.OrderDate)),
+	CASE
+		WHEN LAG(SUM(SOD.LineTotal)) OVER (PARTITION BY P.ProductID ORDER BY YEAR(SOH.OrderDate)) IS NOT NULL THEN
+			(SUM(SOD.LineTotal) - LAG(SUM(SOD.LineTotal)) OVER (PARTITION BY P.ProductID ORDER BY YEAR(SOH.OrderDate)))
+			 * 100.0 / LAG(SUM(SOD.LineTotal)) OVER (PARTITION BY P.ProductID ORDER BY YEAR(SOH.OrderDate))
+		ELSE
+			NULL
+	END AS 'Procentowa różnica sprzedaży'
 FROM Production.Product as P
 	JOIN Sales.SalesOrderDetail AS SOD
 	ON P.ProductID = SOD.ProductID
 	JOIN Sales.SalesOrderHeader AS SOH
 	ON SOD.SalesOrderID = SOH.SalesOrderID
-GROUP BY P.Name, DATEPART(year, SOH.OrderDate)
-ORDER BY P.Name, DATEPART(year, SOH.OrderDate)
+GROUP BY P.Name, P.ProductID, YEAR(SOH.OrderDate)
+ORDER BY P.Name, YEAR(SOH.OrderDate) DESC
+-- najpierw zrobiłem tym spodobem a potem się nauczyłem tej klauzuli WITH więc zostawiłem 2 rozwiązania
+-- oba są poprawne
+
 --
+WITH RocznaSprzedaz AS (
+	SELECT 
+		P.Name AS ProductName,
+		YEAR(SOH.OrderDate) AS Rok,
+		SUM(SOD.LineTotal) AS TotalSales
+	FROM Production.Product as P
+		JOIN Sales.SalesOrderDetail AS SOD
+		ON P.ProductID = SOD.ProductID
+		JOIN Sales.SalesOrderHeader AS SOH
+		ON SOD.SalesOrderID = SOH.SalesOrderID
+	GROUP BY P.Name, YEAR(SOH.OrderDate)
+)
+SELECT
+	ProductName,
+	Rok,
+	--TotalSales,
+	--LAG(SUM(SOD.OrderQty)) OVER (PARTITION BY P.ProductID ORDER BY YEAR(SOH.OrderDate)),
+	CASE
+		WHEN LAG(TotalSales) OVER (PARTITION BY ProductName ORDER BY Rok) IS NOT NULL THEN
+			(TotalSales - LAG(TotalSales) OVER (PARTITION BY ProductName ORDER BY Rok))
+			 * 100.0 / LAG(TotalSales) OVER (PARTITION BY ProductName ORDER BY Rok)
+		ELSE
+			NULL
+	END AS 'Procentowa różnica sprzedaży'
+FROM 
+	RocznaSprzedaz as curr
+ORDER BY ProductName, Rok DESC
+--
+
 
 -- zad. 8
 SELECT
@@ -162,15 +202,48 @@ ORDER BY MAX(SOD.UnitPrice) * 100.0 / MIN(SOD.UnitPrice) DESC
 -- zad. 13
 SELECT
 	P.Name,
-	AVG(DATEDIFF(day, POH.OrderDate, SOH.ShipDate)) AS 'Srednia ilosc dni od zakupu od dowstawcy do sprzedazy klientowi'
+	AVG(DATEDIFF(day, POH.OrderDate, SOH.OrderDate)) AS 'Srednia ilosc dni od zakupu od dowstawcy do sprzedazy klientowi'
 FROM Production.Product AS P
 	JOIN Purchasing.PurchaseOrderDetail AS POD
-	ON POD.ProductID = P.ProductID
+	ON P.ProductID = POD.ProductID
 	JOIN Purchasing.PurchaseOrderHeader AS POH
-	ON POH.PurchaseOrderID = POD.PurchaseOrderID
+	ON POD.PurchaseOrderID = POH.PurchaseOrderID
 	JOIN Sales.SalesOrderDetail AS SOD
-	ON SOD.ProductID = P.ProductID
+	ON P.ProductID = SOD.ProductID
 	JOIN Sales.SalesOrderHeader AS SOH
-	ON SOH.SalesOrderID = SOD.SalesOrderID
+	ON SOD.SalesOrderID = SOH.SalesOrderID
+WHERE POH.OrderDate <= SOH.OrderDate
 GROUP BY P.Name
 ORDER BY P.Name
+
+
+-- zad. 14
+SELECT
+	SUM(case when C.StoreID is NULL then 1 else 0 end) * 100.0 / COUNT(*) as 'Klient firmowy',
+	SUM(case when C.StoreID is NULL then 0 else 1 end) * 100.0 / COUNT(*) as 'Klient indywidualny'
+FROM Sales.Customer AS C
+	JOIN Sales.SalesOrderHeader AS SOH
+	ON C.CustomerID = SOH.CustomerID
+--
+
+-- zad. 15
+SELECT
+	TerritoryName,
+	ProductName,
+	Quantity
+FROM (
+	SELECT
+		ST.Name AS TerritoryName,
+		P.Name AS ProductName,
+		SUM(SOD.OrderQty) AS Quantity,
+		ROW_NUMBER() OVER (PARTITION BY ST.Name ORDER BY SUM(SOD.OrderQty) DESC) AS rn
+	FROM Sales.SalesOrderDetail AS SOD
+		JOIN Sales.SalesOrderHeader AS SOH
+		ON SOD.SalesOrderID = SOH.SalesOrderID
+		JOIN Sales.SalesTerritory AS ST
+		ON SOH.TerritoryID = ST.TerritoryID
+		JOIN Production.Product AS P
+		ON SOD.ProductID = P.ProductID
+	GROUP BY ST.Name, P.Name
+) sub
+WHERE rn = 1
