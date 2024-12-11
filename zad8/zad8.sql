b226033cd5e77cd25edd3fc4e0435b42fdc7ca14
LeoVLO <borowiczl07e@lo5.bielsko.pl>
2024-12-11 13:07:55 +0100

    Fixed zad8.sql

--- zad8/zad8.sql
+++ zad8/zad8.sql
@@ -6,9 +6,15 @@ DELETE FROM Sales.Customer WHERE CustomerID = 50000
 DELETE FROM Sales.SalesOrderHeader WHERE SalesOrderID = 75123
 --
 
+BEGIN TRANSACTION;
 -- zad 3
-DELETE FROM Production.Product WHERE Name = 'Bikes' AND ProductSubcategoryID = 1
+INSERT INTO Production.Product (Name, ProductNumber, SafetyStockLevel, ReorderPoint, StandardCost, ListPrice, DaysToManufacture, SellStartDate, ProductSubcategoryID)
+	VALUES ('Rower', 'RW-0007', 1, 1, 1, 1, 1, GETDATE(), 1)
+SELECT * FROM Production.Product WHERE Name = 'Rower'
+DELETE FROM Production.Product WHERE ProductSubcategoryID = 1 AND Name = 'Rower'
 -- 
+ROLLBACK;
+
 
 -- zad 4
 DELETE FROM Sales.SalesOrderHeader WHERE OrderDate < '01-01-2022'
@@ -22,9 +28,14 @@ DELETE FROM HumanResources.Employee WHERE BusinessEntityID = 3000
 DELETE FROM Sales.SalesOrderHeader WHERE CustomerID = 30000
 --
 
+
+BEGIN TRANSACTION;
 -- zad 7
+INSERT INTO Production.Product (Name, ProductNumber, SafetyStockLevel, ReorderPoint, StandardCost, ListPrice, DaysToManufacture, SellStartDate)
+	VALUES ('Rower', 'RW-0007', 1, 1, 1, 10001, 1, GETDATE())
 DELETE FROM Production.Product WHERE ListPrice > 10000
 --
+ROLLBACK;
 
 -- zad 8
 DELETE FROM HumanResources.Employee WHERE BusinessEntityID NOT IN 
@@ -34,7 +45,8 @@ DELETE FROM HumanResources.Employee WHERE BusinessEntityID NOT IN
 --
 
 -- zad 9
-SELECT * FROM 
+BEGIN TRANSACTION;
+DELETE Sales.SalesOrderDetail FROM 
 
     Sales.SalesOrderDetail AS SOD
     JOIN Production.Product AS P
@@ -45,10 +57,13 @@ SELECT * FROM
     ON PS.ProductCategoryID = PC.ProductCategoryID
 
 WHERE PC.ProductCategoryID = 2
+ROLLBACK;
 --
 -- w treści zadania błąd
 -- ProductCategoryID = 3 nie odpowiada 'Components', ID = 2 to 'Components'
 
 -- zad 10
+BEGIN TRANSACTION;
 DELETE FROM Person.Person WHERE LastName LIKE 'Test%'
+ROLLBACK;
 --
