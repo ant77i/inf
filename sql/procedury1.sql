46bab0c25796b2a05f31262e7b0a395903464e1b
LeoVLO <borowiczl07e@lo5.bielsko.pl>
2024-12-18 12:54:28 +0100

    Updated procedury tutorial

--- sql/procedury1.sql
+++ sql/procedury1.sql
@@ -13,4 +13,44 @@ CREATE PROCEDURE GetCustomerOrders
 		WHERE
 			CustomerID = @CustomerID;
 	END;
+GO
 
+-- procedura 2 aktualizowanie pracownika
+CREATE PROCEDURE UpdateEmployeeInfo
+	@BusinessEntityID INT,
+	@JobTitle NVARCHAR(50),
+	@HireDate DATE
+AS
+BEGIN
+	UPDATE HumanResources.Employee
+	SET
+		JobTitle = @JobTitle,
+		HireDate = @HireDate,
+		ModifiedDate = GETDATE()
+	WHERE
+		BusinessEntityID = @BusinessEntityID;
+END;
+GO
+
+-- modyfikacja procedury
+ALTER PROCEDURE UpdateEmployeeInfo
+	@BusinessEntityID INT,
+	@JobTitle NVARCHAR(50),
+	@Gender NCHAR(1),
+	@HireDate DATE
+AS
+BEGIN
+	UPDATE HumanResources.Employee
+	SET
+		JobTitle = @JobTitle,
+		HireDate = @HireDate,
+		Gender = @Gender,
+		ModifiedDate = GETDATE()
+	WHERE
+		BusinessEntityID = @BusinessEntityID;
+END;
+GO
+
+IF OBJECT_ID('UpdateEmployeeInfo') IS NOT NULL
+	DROP PROCEDURE UpdateEmployeeInfo;
+GO
---
+++ sql/wywoływanie_procedur.sql
@@ -0,0 +1,5 @@
+EXEC GetCustomerOrders @CustomerID = 29825
+
+EXEC UpdateEmployeeInfo @BusinessEntityID = 1, @JobTitle = 'Design Engineer', @HireDate = '2008-01-06'
+
+SELECT * FROM HumanResources.Employee
