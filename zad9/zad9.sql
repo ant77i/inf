91c63ac133e2c874278479b7f4860eb1dc904168
LeoVLO <borowiczl07e@lo5.bielsko.pl>
2024-12-19 07:57:13 +0100

    Updated zad9

--- zad9/zad9.sql
+++ zad9/zad9.sql
@@ -55,3 +55,35 @@ BEGIN
 		WHERE BusinessEntityID = @ID;
 END;
 GO
+--
+
+-- zad 5
+CREATE PROCEDURE DeletePerson
+	@ID INT
+AS
+BEGIN
+	IF @ID IN (SELECT BusinessEntityID FROM HumanResources.Employee)
+		DELETE HumanResources.Employee WHERE BusinessEntityID = @ID;
+	IF @ID IN (SELECT BusinessEntityID FROM Person.BusinessEntityContact)
+		DELETE Person.BusinessEntityContact WHERE BusinessEntityID = @ID;
+	IF @ID IN (SELECT BusinessEntityID FROM Person.EmailAddress)
+		DELETE Person.EmailAddress WHERE BusinessEntityID = @ID;
+	IF @ID IN (SELECT BusinessEntityID FROM Person.Password)
+		DELETE Person.Password WHERE BusinessEntityID = @ID;
+	IF @ID IN (SELECT BusinessEntityID FROM Person.PersonPhone)
+		DELETE Person.PersonPhone WHERE BusinessEntityID = @ID;
+	IF @ID IN (SELECT PersonID FROM Sales.Customer)
+		DELETE Sales.Customer WHERE PersonID = @ID;
+	IF @ID IN (SELECT BusinessEntityID FROM Sales.PersonCreditCard)
+		DELETE Sales.PersonCreditCard WHERE BusinessEntityID = @ID;
+
+	IF @ID IN (SELECT BusinessEntityID FROM Person.Person)
+		DELETE Person.Person WHERE BusinessEntityID = @ID;
+END;
+GO
+
+BEGIN TRANSACTION
+EXEC DeletePerson @ID = 309
+ROLLBACK;
+
+SELECT * FROM Person.Person
